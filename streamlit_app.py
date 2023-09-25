fcf425634c129ce08a0f35847b0949e9a22563ac
aws-antonio-hernandez <142955790+aws-antonio-hernandez@users.noreply.github.com>
2023-09-25 15:28:23 +0800

    Update streamlit_app.py
    Added printing

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,8 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
+fruitychoice_normalized = pandas.json_normalize(fruitychoice_response.json())
+streamlit.dataframe(fruitychoice_normalized)
 
 
 
