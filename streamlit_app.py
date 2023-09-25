7b7788eba4dba7e94d7a29efadc0ea365de3409e
aws-antonio-hernandez <142955790+aws-antonio-hernandez@users.noreply.github.com>
2023-09-25 15:29:31 +0800

    Update streamlit_app.py
    Corrected bug: mistyped text

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruitychoice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruitychoice_normalized = pandas.json_normalize(fruitychoice_response.json())
 streamlit.dataframe(fruitychoice_normalized)
 
