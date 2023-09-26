4eac4d13193995bb15c6fc60aa5f6e8336508ad5
aws-antonio-hernandez <142955790+aws-antonio-hernandez@users.noreply.github.com>
2023-09-26 18:47:10 +0800

    Update streamlit_app.py
    adding box

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,7 @@ streamlit.header("The fruit load list contains:")
 #streamlit.text(my_data_rows)
 streamlit.dataframe(my_data_rows)
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+streamlit.write('Thanks for adding jackfruit')
+
 
