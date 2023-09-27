aec7982038df355d0db7db8d16f68c9128eccd08
aws-antonio-hernandez <142955790+aws-antonio-hernandez@users.noreply.github.com>
2023-09-27 12:42:06 +0800

    Update streamlit_app.py
    insert try/except

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,23 +26,27 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-#streamlit.text(fruityvice_response.json())
+try:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+  #streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+  # write your own comment -what does the next line do? 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # write your own comment - what does this do?
+  streamlit.dataframe(fruityvice_normalized)
 
-# New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+  # New Section to display fruityvice api response
+  streamlit.header('Fruityvice Fruit Advice!')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  streamlit.write('The user entered ', fruit_choice)
 
-fruitychoice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruitychoice_normalized = pandas.json_normalize(fruitychoice_response.json())
-streamlit.dataframe(fruitychoice_normalized)
+  fruitychoice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruitychoice_normalized = pandas.json_normalize(fruitychoice_response.json())
+  streamlit.dataframe(fruitychoice_normalized)
 
+except URLError as e:
+  streamlit.error()
+  
 # dont run anything past here while we troubleshoot
 streamlit.stop()
 
