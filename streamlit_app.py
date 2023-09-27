7fa6efd0f4234bff7be4507bfba4d6fec1c99bad
aws-antonio-hernandez <142955790+aws-antonio-hernandez@users.noreply.github.com>
2023-09-27 14:15:08 +0800

    Update streamlit_app.py
    Adding another function insert_row_snowflakes.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,11 @@ def get_fruit_load_list():
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
+def insert_row_snowflakes(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu') 
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
@@ -64,6 +69,12 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?)
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_fucntion = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
 
 # dont run anything past here while we troubleshoot
 streamlit.stop()
